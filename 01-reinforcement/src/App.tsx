b1b7d0f9f34b4c13e631c86066ad146e85cc867d
davidivad96 <davidivad96@gmail.com>
2021-07-07 22:25:15 +0200

    BasicTypes

--- 01-reinforcement/src/App.tsx
+++ 01-reinforcement/src/App.tsx
@@ -1,7 +1,11 @@
+import { BasicTypes } from "./typescript/BasicTypes";
+
 export const App = () => {
   return (
     <div className="mt-2">
       <h1>Introduction to TS React</h1>
+      <hr />
+      <BasicTypes />
     </div>
   );
 };
---
+++ 01-reinforcement/src/typescript/BasicTypes.tsx
@@ -0,0 +1,16 @@
+export const BasicTypes = () => {
+  const name: string = "David";
+  const age: number = 25;
+  const isActive: boolean = false;
+
+  const powers: string[] = ["Velocity", "Fly", "Breath under water"];
+
+  return (
+    <>
+      <h3>Basic Types</h3>
+      {name}, {age}, {isActive ? "active" : "no active"}
+      <br />
+      {powers.join(", ")}
+    </>
+  );
+};
