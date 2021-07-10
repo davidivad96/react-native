06137c6f7fe8a730d14fb44e2eacf712ee4aa140
davidivad96 <davidivad96@gmail.com>
2021-07-10 18:31:17 +0200

    Wrapped app in SafeAreaView component

--- 02-CounterApp/App.tsx
+++ 02-CounterApp/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+// import { HelloWorld } from './src/screens/HelloWorld';
 import { Counter } from './src/screens/Counter';
 
 const App = () => {
-  return <Counter />;
+  return (
+    <SafeAreaView style={styles.root}>
+      {/* <HelloWorld /> */}
+      <Counter />
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
 
 export default App;
