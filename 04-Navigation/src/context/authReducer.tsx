74f29d9717d7b0f83110dfa1628c5a1293de351b
davidivad96 <davidivad96@gmail.com>
2021-07-19 20:03:09 +0200

    Create basic auth reducer

---
+++ 04-Navigation/src/context/authReducer.tsx
@@ -0,0 +1,20 @@
+import { AuthState } from './Auth';
+
+type AuthAction = {
+  type: 'signIn';
+};
+
+export const authReducer = (
+  state: AuthState,
+  action: AuthAction,
+): AuthState => {
+  switch (action.type) {
+    case 'signIn':
+      return {
+        ...state,
+        isLoggedIn: true,
+      };
+    default:
+      return state;
+  }
+};
