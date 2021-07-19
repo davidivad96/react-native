ccbfa577dc8e681b8152cc0a75df26f4ad64c1f1
davidivad96 <davidivad96@gmail.com>
2021-07-19 19:12:43 +0200

    Test RN vector icons

--- 04-Navigation/src/screens/Tab1.tsx
+++ 04-Navigation/src/screens/Tab1.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-import { styles } from '../themes/appTheme';
+import { styles, colors } from '../themes/appTheme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export const Tab1 = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View style={styles.globalMargin}>
-      <Text style={styles.title}>Tab1</Text>
-      <Icon name="airplane-outline" size={60} color="#900" />
+    <View style={{ ...styles.globalMargin, marginTop: top + 10 }}>
+      <Text style={styles.title}>Icons</Text>
+      <Icon name="airplane-outline" size={60} color={colors.primary} />
+      <Icon name="attach-outline" size={60} color={colors.primary} />
+      <Icon name="bonfire-outline" size={60} color={colors.primary} />
+      <Icon name="calculator-outline" size={60} color={colors.primary} />
+      <Icon
+        name="chatbubble-ellipses-outline"
+        size={60}
+        color={colors.primary}
+      />
     </View>
   );
 };
