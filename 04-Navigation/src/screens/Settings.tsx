43ac11d3da3acddfa23039dd935ac70235f376cf
davidivad96 <davidivad96@gmail.com>
2021-07-19 19:55:05 +0200

    Consume Auth context in Settings screen

--- 04-Navigation/src/screens/Settings.tsx
+++ 04-Navigation/src/screens/Settings.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { styles } from '../themes/appTheme';
+import { AuthContext } from '../context/Auth';
 
 export const Settings = () => {
+  const { authState } = useContext(AuthContext);
   const insets = useSafeAreaInsets();
 
   return (
     <View style={{ ...styles.globalMargin, marginTop: insets.top }}>
       <Text style={styles.title}>Settings screen</Text>
+      <Text>{JSON.stringify(authState)}</Text>
     </View>
   );
 };
