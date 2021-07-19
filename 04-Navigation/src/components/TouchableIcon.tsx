1fd562ac998970e27d029008f2ecfb646c215f87
davidivad96 <davidivad96@gmail.com>
2021-07-19 20:21:42 +0200

    Change favorite icon when pressing on it, and display favorite icon in the Settings screen

--- 04-Navigation/src/components/TouchableIcon.tsx
+++ 04-Navigation/src/components/TouchableIcon.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { colors } from '../themes/appTheme';
+import { AuthContext } from '../context/Auth';
 
 interface Props {
   iconName: string;
 }
 
-export const TouchableIcon = ({ iconName }: Props) => (
-  <TouchableOpacity>
-    <Icon name={iconName} size={80} color={colors.primary} />
-  </TouchableOpacity>
-);
+export const TouchableIcon = ({ iconName }: Props) => {
+  const { changeFavoriteIcon } = useContext(AuthContext);
+
+  return (
+    <TouchableOpacity onPress={() => changeFavoriteIcon(iconName)}>
+      <Icon name={iconName} size={80} color={colors.primary} />
+    </TouchableOpacity>
+  );
+};
--- 04-Navigation/src/screens/Settings.tsx
+++ 04-Navigation/src/screens/Settings.tsx
@@ -1,7 +1,8 @@
 import React, { useContext } from 'react';
 import { Text, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { styles } from '../themes/appTheme';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { styles, colors } from '../themes/appTheme';
 import { AuthContext } from '../context/Auth';
 
 export const Settings = () => {
@@ -12,6 +13,9 @@ export const Settings = () => {
     <View style={{ ...styles.globalMargin, marginTop: insets.top }}>
       <Text style={styles.title}>Settings screen</Text>
       <Text>{JSON.stringify(authState)}</Text>
+      {authState.favoriteIcon && (
+        <Icon name={authState.favoriteIcon} size={150} color={colors.primary} />
+      )}
     </View>
   );
 };
