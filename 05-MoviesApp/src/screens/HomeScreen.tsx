2c997822c67e27367327a84d4c0a175c1a92b4f2
davidivad96 <davidivad96@gmail.com>
2021-07-20 20:27:13 +0200

    Use interfaces in our Home screen

--- 05-MoviesApp/src/screens/HomeScreen.tsx
+++ 05-MoviesApp/src/screens/HomeScreen.tsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { View, Text } from 'react-native';
 import movieDB from '../api/movieDB';
+import { MovieDBNowPlaying } from '../interfaces/movieDBInterfaces';
 
 export const HomeScreen = () => {
   useEffect(() => {
     movieDB
-      .get('/now_playing')
-      .then(res => console.log('res: ', res.data.total_pages))
+      .get<MovieDBNowPlaying>('/now_playing')
+      .then(res => console.log('res: ', res.data.results[0].title))
       .catch(err => console.log(err));
   }, []);
 
