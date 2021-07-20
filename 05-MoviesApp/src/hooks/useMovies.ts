012261906ac566e82022beafe250d5a74bf6d095
davidivad96 <davidivad96@gmail.com>
2021-07-20 20:42:50 +0200

    Implement useMovies hook

---
+++ 05-MoviesApp/src/hooks/useMovies.ts
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import movieDB from '../api/movieDB';
+import { Movie, MovieDBNowPlaying } from '../interfaces/movieDBInterfaces';
+
+export const useMovies = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const getMovies = async () => {
+    const res = await movieDB.get<MovieDBNowPlaying>('/now_playing');
+    setMovies(res.data.results);
+    setIsLoading(true);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return { movies, isLoading };
+};
