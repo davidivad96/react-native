5c7d336a5863b6fef0484d3f556c8309c6f57f6c
davidivad96 <davidivad96@gmail.com>
2021-07-22 21:10:27 +0200

    Implement HorizontalSlider component

---
+++ 05-MoviesApp/src/components/HorizontalSlider.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { Movie } from '../interfaces/movieDBInterfaces';
+import { MoviePoster } from './MoviePoster';
+
+interface Props {
+  title?: string;
+  movies: Movie[];
+}
+
+export const HorizontalSlider = ({ title, movies }: Props) => {
+  return (
+    <View style={title ? styles.container : styles.containerWithoutTitle}>
+      {title && <Text style={styles.title}>{title}</Text>}
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => (
+          <MoviePoster movie={item} width={140} height={200} />
+        )}
+        keyExtractor={item => item.id.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 260,
+  },
+  containerWithoutTitle: {
+    height: 220,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingHorizontal: 10,
+  },
+});
