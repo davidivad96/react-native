25296935d39eb19c9eaf676e5bb6131fd154d6f2
davidivad96 <davidivad96@gmail.com>
2021-07-22 21:26:04 +0200

    make 4 API requests at the same time and to make it more efficient

--- 05-MoviesApp/src/hooks/useMovies.ts
+++ 05-MoviesApp/src/hooks/useMovies.ts
@@ -2,18 +2,39 @@ import { useEffect, useState } from 'react';
 import movieDB from '../api/movieDB';
 import { Movie, MovieDBMoviesResponse } from '../interfaces/movieDBInterfaces';
 
+interface MoviesState {
+  nowPlaying: Movie[];
+  popular: Movie[];
+  topRated: Movie[];
+  upcoming: Movie[];
+}
+
 export const useMovies = () => {
-  const [nowPlayingMovies, setNowPlayingMovies] = useState<Movie[]>([]);
-  const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
+  const [moviesState, setMoviesState] = useState<MoviesState>({
+    nowPlaying: [],
+    popular: [],
+    topRated: [],
+    upcoming: [],
+  });
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   const getMovies = async () => {
-    const resNowPlaying = await movieDB.get<MovieDBMoviesResponse>(
-      '/now_playing',
-    );
-    setNowPlayingMovies(resNowPlaying.data.results);
-    const resPopular = await movieDB.get<MovieDBMoviesResponse>('/popular');
-    setPopularMovies(resPopular.data.results);
+    const nowPlaying = movieDB.get<MovieDBMoviesResponse>('/now_playing');
+    const popular = movieDB.get<MovieDBMoviesResponse>('/popular');
+    const topRated = movieDB.get<MovieDBMoviesResponse>('/top_rated');
+    const upcoming = movieDB.get<MovieDBMoviesResponse>('/upcoming');
+    const response = await Promise.all([
+      nowPlaying,
+      popular,
+      topRated,
+      upcoming,
+    ]);
+    setMoviesState({
+      nowPlaying: response[0].data.results,
+      popular: response[1].data.results,
+      topRated: response[2].data.results,
+      upcoming: response[3].data.results,
+    });
     setIsLoading(false);
   };
 
@@ -21,5 +42,5 @@ export const useMovies = () => {
     getMovies();
   }, []);
 
-  return { nowPlayingMovies, popularMovies, isLoading };
+  return { ...moviesState, isLoading };
 };
