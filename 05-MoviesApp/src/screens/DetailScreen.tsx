8dc21700c359b808f26c29e246fe9ef8cd49c18e
davidivad96 <davidivad96@gmail.com>
2021-07-22 22:09:49 +0200

    First steps on the detail screen

--- 05-MoviesApp/src/screens/DetailScreen.tsx
+++ 05-MoviesApp/src/screens/DetailScreen.tsx
@@ -1,15 +1,72 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {
+  View,
+  Image,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+  Text,
+} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../navigation/MainNavigator';
+
+const { height: screenHeight } = Dimensions.get('screen');
 
 interface Props extends StackScreenProps<RootStackParams, 'Detail'> {}
 
 export const DetailScreen = ({ route }: Props) => {
   const movie = route.params;
+  const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+
   return (
-    <View>
-      <Text>Detail Screen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <View style={styles.imageBorder}>
+          <Image source={{ uri }} style={styles.posterImage} />
+        </View>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.subtitle}>{movie.original_title}</Text>
+        <Text style={styles.title}>{movie.title}</Text>
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: screenHeight * 0.7,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity: 0.45,
+    shadowRadius: 9.5,
+    elevation: 15,
+    borderBottomEndRadius: 25,
+    borderBottomStartRadius: 25,
+  },
+  imageBorder: {
+    flex: 1,
+    overflow: 'hidden',
+    borderBottomEndRadius: 25,
+    borderBottomStartRadius: 25,
+  },
+  posterImage: {
+    flex: 1,
+  },
+  textContainer: {
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    fontSize: 16,
+    opacity: 0.4,
+  },
+});
