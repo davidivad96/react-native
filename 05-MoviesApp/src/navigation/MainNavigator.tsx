3e959e83b8042c253803a1ae8f1c5b75af6b41eb
davidivad96 <davidivad96@gmail.com>
2021-07-22 21:52:25 +0200

    RootStackParams type

--- 05-MoviesApp/src/navigation/MainNavigator.tsx
+++ 05-MoviesApp/src/navigation/MainNavigator.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/HomeScreen';
 import { DetailScreen } from '../screens/DetailScreen';
+import { Movie } from '../interfaces/movieDBInterfaces';
+
+export type RootStackParams = {
+  Home: undefined;
+  Detail: Movie;
+};
 
 const Stack = createStackNavigator();
 
