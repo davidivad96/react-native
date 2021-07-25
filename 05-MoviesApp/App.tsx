1d161d2bb614eae4fb68e9161df78bb9d509e2ce
davidivad96 <davidivad96@gmail.com>
2021-07-25 19:55:18 +0200

    Use useFade hook to test fadeIn and fadeOut animations

--- 05-MoviesApp/App.tsx
+++ 05-MoviesApp/App.tsx
@@ -2,11 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { MainNavigator } from './src/navigation/MainNavigator';
+// import { FadeScreen } from './src/screens/FadeScreen';
 
 const App = () => {
   return (
     <NavigationContainer>
       <MainNavigator />
+      {/* <FadeScreen /> */}
     </NavigationContainer>
   );
 };
---
+++ 05-MoviesApp/src/screens/FadeScreen.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, StyleSheet, Animated, Button } from 'react-native';
+import { useFade } from '../hooks/useFade';
+
+export const FadeScreen = () => {
+  const { opacity, fadeIn, fadeOut } = useFade(0);
+
+  return (
+    <View style={styles.container}>
+      <Animated.View style={[styles.box, { opacity }]} />
+      <Button title="Fade In!" onPress={fadeIn} />
+      <Button title="Fade Out!" onPress={fadeOut} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    backgroundColor: '#084F6A',
+    width: 150,
+    height: 150,
+    borderColor: 'white',
+    borderWidth: 10,
+  },
+});
