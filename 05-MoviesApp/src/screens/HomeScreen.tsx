6b490a2404365014f7eda922e017d8ad72d507ff
davidivad96 <davidivad96@gmail.com>
2021-07-25 20:10:54 +0200

    Implement and use GradientBackground component

--- 05-MoviesApp/src/screens/HomeScreen.tsx
+++ 05-MoviesApp/src/screens/HomeScreen.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
+import { GradientBackground } from '../components/GradientBackground';
 import { HorizontalSlider } from '../components/HorizontalSlider';
 import { MoviePoster } from '../components/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
@@ -27,22 +28,24 @@ export const HomeScreen = () => {
   }
 
   return (
-    <ScrollView>
-      <View style={{ paddingTop: top + 20 }}>
-        <View style={styles.carouselContainer}>
-          <Carousel
-            data={nowPlaying}
-            renderItem={({ item }) => <MoviePoster movie={item} />}
-            sliderWidth={windowWith}
-            itemWidth={300}
-            inactiveSlideOpacity={0.5}
-          />
+    <GradientBackground>
+      <ScrollView>
+        <View style={{ paddingTop: top + 20 }}>
+          <View style={styles.carouselContainer}>
+            <Carousel
+              data={nowPlaying}
+              renderItem={({ item }) => <MoviePoster movie={item} />}
+              sliderWidth={windowWith}
+              itemWidth={300}
+              inactiveSlideOpacity={0.5}
+            />
+          </View>
+          <HorizontalSlider title="Popular" movies={popular} />
+          <HorizontalSlider title="Top Rated" movies={topRated} />
+          <HorizontalSlider title="Upcoming" movies={upcoming} />
         </View>
-        <HorizontalSlider title="Popular" movies={popular} />
-        <HorizontalSlider title="Top Rated" movies={topRated} />
-        <HorizontalSlider title="Upcoming" movies={upcoming} />
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </GradientBackground>
   );
 };
 
