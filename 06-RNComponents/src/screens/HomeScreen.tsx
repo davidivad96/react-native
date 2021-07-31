eb3002b4cc310476be06cc2e34bbc217b0388ea0
davidivad96 <davidivad96@gmail.com>
2021-07-31 22:06:03 +0200

    Install and configure react-native-vetor-icons

--- 06-RNComponents/src/screens/HomeScreen.tsx
+++ 06-RNComponents/src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const HomeScreen = () => {
   return (
     <View>
       <Text>Home screen</Text>
+      <Icon name="airplane-outline" size={40} color="red" />
     </View>
   );
 };
