de38a3ad7de82b7eb538b7678419564f6200af2b
davidivad96 <davidivad96@gmail.com>
2021-07-31 22:53:27 +0200

    Make FlatListMenuItem component touchable

--- 06-RNComponents/src/components/FlatListMenuItem.tsx
+++ 06-RNComponents/src/components/FlatListMenuItem.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { MenuItem } from '../interfaces/appInterfaces';
+import { Spacer } from './Spacer';
 
 interface Props {
   item: MenuItem;
 }
 
 export const FlatListMenuItem = ({ item }: Props) => (
-  <View style={styles.container}>
+  <TouchableOpacity style={styles.container} activeOpacity={0.2}>
     <Icon name={item.icon} color="grey" size={25} />
     <Text style={styles.text}>{item.name}</Text>
-  </View>
+    <Spacer />
+    <Icon name="chevron-forward-outline" color="grey" size={25} />
+  </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
