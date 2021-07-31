1accc5e99d5bab70eed826d19370781f99e35f08
davidivad96 <davidivad96@gmail.com>
2021-07-31 22:34:07 +0200

    Add ListHeaderComponent and ItemSeparatorComponent to FlatList

--- 06-RNComponents/src/screens/HomeScreen.tsx
+++ 06-RNComponents/src/screens/HomeScreen.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, FlatList, Text } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { appTheme } from '../theme/appTheme';
 
 interface MenuItem {
   id: number;
@@ -15,20 +17,38 @@ const DATA: MenuItem[] = [
     icon: 'cube-outline',
     components: 'Animation101Screen',
   },
+  {
+    id: 1,
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    components: 'Animation102Screen',
+  },
 ];
 
 export const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
+
   const renderItem = (item: MenuItem) => (
     <Text>
       {item.name} - {item.icon}
     </Text>
   );
 
+  const renderListHeader = () => (
+    <Text style={appTheme.title}>Menu Options</Text>
+  );
+
+  const renderSeparator = () => <View style={styles.separator} />;
+
   return (
-    <View style={styles.container}>
+    <View
+      style={[styles.container, appTheme.container, { marginTop: top + 20 }]}
+    >
       <FlatList
         data={DATA}
         renderItem={({ item }) => renderItem(item)}
+        ListHeaderComponent={renderListHeader}
+        ItemSeparatorComponent={renderSeparator}
         keyExtractor={item => item.id.toString()}
       />
     </View>
@@ -38,5 +58,10 @@ export const HomeScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  separator: {
+    borderBottomWidth: 2,
+    opacity: 0.4,
+    marginVertical: 3,
   },
 });
---
+++ 06-RNComponents/src/theme/appTheme.ts
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const appTheme = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+});
