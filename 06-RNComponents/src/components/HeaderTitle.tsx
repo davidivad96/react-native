54d57ade8e0d52541351385b4ed2cb1c5ef2228e
davidivad96 <davidivad96@gmail.com>
2021-08-11 19:31:08 +0200

    Add style prop to HeaderTitle component

--- 06-RNComponents/src/components/HeaderTitle.tsx
+++ 06-RNComponents/src/components/HeaderTitle.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleProp, Text, TextStyle } from 'react-native';
 import { appTheme } from '../theme/appTheme';
 
 interface Props {
   text: string;
   color?: string;
+  style?: StyleProp<TextStyle>;
 }
 
-export const HeaderTitle = ({ text, color = '#000' }: Props) => (
-  <Text style={[appTheme.title, { color }]}>{text}</Text>
+export const HeaderTitle = ({ text, color = '#000', style = {} }: Props) => (
+  <Text style={[appTheme.title, { color, ...(style as TextStyle) }]}>
+    {text}
+  </Text>
 );
