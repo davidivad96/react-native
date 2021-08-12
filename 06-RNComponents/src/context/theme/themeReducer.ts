9a0906100c0f7d4981492ae81fc11a20564ec604
davidivad96 <davidivad96@gmail.com>
2021-08-12 22:57:25 +0200

    Implement theme reducer

---
+++ 06-RNComponents/src/context/theme/themeReducer.ts
@@ -0,0 +1,50 @@
+import { Theme } from '@react-navigation/native';
+
+type ThemeAction = { type: 'set_light_theme' } | { type: 'set_dark_theme' };
+
+export interface ThemeState extends Theme {
+  currentTheme: 'light' | 'dark';
+  dividerColor: string;
+}
+
+export const lightTheme: ThemeState = {
+  currentTheme: 'light',
+  dark: false,
+  colors: {
+    primary: '#084F6A',
+    background: '#FFF',
+    card: '#FFF',
+    text: '#000',
+    border: '#000',
+    notification: '#008080',
+  },
+  dividerColor: 'rgba(0,0,0,0.6)',
+};
+
+export const darkTheme: ThemeState = {
+  currentTheme: 'dark',
+  dark: true,
+  colors: {
+    primary: '#75CEDB',
+    background: '#000',
+    card: '#000',
+    text: '#FFF',
+    border: '#FFF',
+    notification: '#008080',
+  },
+  dividerColor: 'rgba(255,255,255,0.6)',
+};
+
+export const themeReducer = (
+  state: ThemeState,
+  action: ThemeAction,
+): ThemeState => {
+  switch (action.type) {
+    case 'set_light_theme':
+      return { ...lightTheme };
+    case 'set_dark_theme':
+      return { ...darkTheme };
+    default:
+      return state;
+  }
+};
