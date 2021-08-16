e0bb77b5888db4032e923963cf833cd4dc0390d3
davidivad96 <davidivad96@gmail.com>
2021-08-16 20:12:27 +0200

    Add and configure react-native-vector-icons for both, iOS and Android

--- 07-Pokedex/App.tsx
+++ 07-Pokedex/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
   return (
     <View>
       <Text>Hello World!</Text>
+      <Icon name="star-outline" size={25} color="red" />
     </View>
   );
 };
