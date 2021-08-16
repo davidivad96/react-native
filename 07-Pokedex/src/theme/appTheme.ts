1d59177c2a45dbe7eddaf7787549ff37656d2465
davidivad96 <davidivad96@gmail.com>
2021-08-16 20:43:45 +0200

    Add appTheme

---
+++ 07-Pokedex/src/theme/appTheme.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+export const appTheme = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  globalPadding: {
+    padding: 20,
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: 'bold',
+  },
+});
