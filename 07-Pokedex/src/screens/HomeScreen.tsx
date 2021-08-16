fb3af29946db0a18f53c03ea9046df1d40ab195c
davidivad96 <davidivad96@gmail.com>
2021-08-16 20:43:57 +0200

    Improve HomeScreen

--- 07-Pokedex/src/screens/HomeScreen.tsx
+++ 07-Pokedex/src/screens/HomeScreen.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import { Text, View } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { appTheme } from '../theme/appTheme';
+const PokeballImage = require('../assets/pokeball.png');
 
 export const HomeScreen = () => {
+  const { top } = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>Home screen</Text>
-      <Icon name="star-outline" size={25} color="red" />
+    <View style={appTheme.container}>
+      <Image source={PokeballImage} style={styles.pokeball} />
+      <Text style={[appTheme.globalPadding, appTheme.title, { top: top + 20 }]}>
+        Pokedex
+      </Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  pokeball: {
+    position: 'absolute',
+    width: 300,
+    height: 300,
+    top: -100,
+    right: -100,
+    opacity: 0.3,
+  },
+});
