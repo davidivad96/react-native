56f097a06928cf1e6d285acb6609ac7864795086
davidivad96 <davidivad96@gmail.com>
2021-08-17 20:59:19 +0200

    Improve PokemonCard component

--- 07-Pokedex/src/components/PokemonCard.tsx
+++ 07-Pokedex/src/components/PokemonCard.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {
   Dimensions,
+  Image,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
 } from 'react-native';
 import { Pokemon } from '../interfaces/pokemonInterfaces';
+import { FadeInImage } from './FadeInImage';
+const WhitePokeballImage = require('../assets/white-pokeball.png');
 
 const { width: windowWidth } = Dimensions.get('window');
 
@@ -16,11 +19,15 @@ interface Props {
 
 export const PokemonCard = ({ pokemon }: Props) => {
   return (
-    <TouchableOpacity activeOpacity={0.9}>
+    <TouchableOpacity activeOpacity={0.4}>
       <View style={{ ...styles.cardContainer, width: windowWidth * 0.4 }}>
-        <Text
-          style={styles.pokemonName}
-        >{`${pokemon.name}\n#${pokemon.id}`}</Text>
+        <Text style={styles.pokemonName}>
+          {`${pokemon.name}\n#${pokemon.id}`}
+        </Text>
+        <View style={styles.pokeballContainer}>
+          <Image source={WhitePokeballImage} style={styles.pokeball} />
+        </View>
+        <FadeInImage uri={pokemon.picture} style={styles.pokemonPicture} />
       </View>
     </TouchableOpacity>
   );
@@ -33,6 +40,14 @@ const styles = StyleSheet.create({
     height: 120,
     borderRadius: 15,
     backgroundColor: 'red',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
   },
   pokemonName: {
     color: '#FFF',
@@ -40,5 +55,28 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     top: 20,
     left: 10,
+  },
+  pokeballContainer: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    bottom: 0,
+    right: 0,
+    opacity: 0.6,
+    overflow: 'hidden',
+  },
+  pokeball: {
+    width: 100,
+    height: 100,
+    position: 'absolute',
+    right: -20,
+    bottom: -20,
+  },
+  pokemonPicture: {
+    width: 120,
+    height: 120,
+    position: 'absolute',
+    right: -8,
+    bottom: -5,
   },
 });
