36fd78a5df4ab32e35038402e5b21758988d67b7
davidivad96 <davidivad96@gmail.com>
2021-08-17 21:57:32 +0200

    Define Stack navigator routes params

--- 07-Pokedex/src/navigation/StackNavigator.tsx
+++ 07-Pokedex/src/navigation/StackNavigator.tsx
@@ -3,21 +3,28 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { HomeScreen } from '../screens/HomeScreen';
 import { PokemonScreen } from '../screens/PokemonScreen';
+import { Pokemon } from '../interfaces/pokemonInterfaces';
 
-const Stack = createStackNavigator();
-
-export const StackNavigator = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        screenOptions={{
-          headerShown: false,
-          cardStyle: { backgroundColor: '#FFF' },
-        }}
-      >
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Pokemon" component={PokemonScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+export type RootStackParams = {
+  Home: undefined;
+  Pokemon: {
+    pokemon: Pokemon;
+    color: string;
+  };
 };
+
+const Stack = createStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => (
+  <NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        cardStyle: { backgroundColor: '#FFF' },
+      }}
+    >
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Pokemon" component={PokemonScreen} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
