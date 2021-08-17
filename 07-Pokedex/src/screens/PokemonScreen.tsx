424977489c3a7ad2ca2143845b44925fbc9a14c3
davidivad96 <davidivad96@gmail.com>
2021-08-17 22:21:23 +0200

    Display pokemon details (or loading spinner) in Pokemon screen

--- 07-Pokedex/src/screens/PokemonScreen.tsx
+++ 07-Pokedex/src/screens/PokemonScreen.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+  ActivityIndicator,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import { RootStackParams } from '../navigation/StackNavigator';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { FadeInImage } from '../components/FadeInImage';
+import { usePokemon } from '../hooks/usePokemon';
+import { PokemonDetails } from '../components/PokemonDetails';
 const WhitePokeballImage = require('../assets/white-pokeball.png');
 
 interface Props extends StackScreenProps<RootStackParams, 'Pokemon'> {}
@@ -15,9 +24,10 @@ export const PokemonScreen = ({ route, navigation }: Props) => {
     pokemon: { id, name, picture },
     color,
   } = route.params;
+  const { isLoading, pokemon } = usePokemon(id);
 
   return (
-    <View>
+    <View style={styles.container}>
       <View style={[styles.headerContainer, { backgroundColor: color }]}>
         <TouchableOpacity
           activeOpacity={0.4}
@@ -32,11 +42,21 @@ export const PokemonScreen = ({ route, navigation }: Props) => {
         <Image source={WhitePokeballImage} style={styles.pokeball} />
         <FadeInImage uri={picture} style={styles.pokemonPicture} />
       </View>
+      {isLoading ? (
+        <View style={styles.activityIndicator}>
+          <ActivityIndicator color={color} size={50} />
+        </View>
+      ) : (
+        <PokemonDetails pokemon={pokemon} />
+      )}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   headerContainer: {
     height: 400,
     zIndex: 999,
@@ -65,5 +85,10 @@ const styles = StyleSheet.create({
     height: 250,
     bottom: -40,
     opacity: 0.7,
+  },
+  activityIndicator: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
