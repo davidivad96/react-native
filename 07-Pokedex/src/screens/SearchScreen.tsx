b5b30b7c3603582c7660f28316af881cb7ff6fd0
davidivad96 <davidivad96@gmail.com>
2021-08-29 15:22:17 +0200

    Improve SearchScreen

--- 07-Pokedex/src/screens/SearchScreen.tsx
+++ 07-Pokedex/src/screens/SearchScreen.tsx
@@ -1,10 +1,76 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PokemonCard } from '../components/PokemonCard';
+import { SearchInput } from '../components/SearchInput';
+import { usePokemonSearch } from '../hooks/usePokemonSearch';
+import { appTheme } from '../theme/appTheme';
+
+const screenWidth = Dimensions.get('window').width;
 
 export const SearchScreen = () => {
+  const { top } = useSafeAreaInsets();
+  const { isFetching, pokemons } = usePokemonSearch();
+
   return (
-    <View>
-      <Text>Search screen</Text>
+    <View style={[appTheme.container, styles.container]}>
+      {isFetching ? (
+        <View style={styles.activityIndicator}>
+          <ActivityIndicator size={50} color="grey" />
+          <Text>Loading...</Text>
+        </View>
+      ) : (
+        <View>
+          <SearchInput
+            style={{
+              ...styles.searchInput,
+              top: Platform.OS === 'ios' ? top : top + 10,
+              width: screenWidth - 40,
+            }}
+          />
+          <FlatList
+            data={pokemons}
+            keyExtractor={pokemon => pokemon.id}
+            renderItem={({ item }) => <PokemonCard pokemon={item} />}
+            ListHeaderComponent={
+              <Text
+                style={[
+                  appTheme.globalPadding,
+                  appTheme.title,
+                  { marginTop: top + 50 },
+                ]}
+              >
+                Pokedex
+              </Text>
+            }
+            showsVerticalScrollIndicator={false}
+            numColumns={2}
+          />
+        </View>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+  },
+  activityIndicator: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  searchInput: {
+    position: 'absolute',
+    zIndex: 1000,
+  },
+});
