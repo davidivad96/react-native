aa7ea43417b1234329b2605e685e67ee829d4752
davidivad96 <davidivad96@gmail.com>
2021-08-30 13:04:09 +0200

    Install and configure react-native-vector-icons for both, iOS and Android platforms

--- 08-RoutesApp/App.tsx
+++ 08-RoutesApp/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
   return (
     <View>
       <Text>Hello World!</Text>
+      <Icon name="airplane-outline" color="blue" size={40} />
     </View>
   );
 };
