083eb738ef76220f6f6cc763b89767e2fe6be3ff
davidivad96 <davidivad96@gmail.com>
2021-08-31 12:18:56 +0200

    Add padding to PermissionsScreen

--- 08-RoutesApp/src/pages/PermissionsScreen.tsx
+++ 08-RoutesApp/src/pages/PermissionsScreen.tsx
@@ -24,6 +24,7 @@ export const PermissionsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 25,
     justifyContent: 'center',
     alignItems: 'center',
   },
