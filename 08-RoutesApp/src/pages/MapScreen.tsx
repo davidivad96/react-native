a7eb03f8fb8ef3ceeeb66b8d74b878b05590ec25
davidivad96 <davidivad96@gmail.com>
2021-08-31 12:42:47 +0200

    Show Google Maps map in MapScreen

--- 08-RoutesApp/src/pages/MapScreen.tsx
+++ 08-RoutesApp/src/pages/MapScreen.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export const MapScreen = () => {
   return (
-    <View>
-      <Text>Map screen</Text>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
