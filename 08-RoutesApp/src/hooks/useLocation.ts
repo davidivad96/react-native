5c5baa29f9147ae860521047e50a8da6a1a31cf7
davidivad96 <davidivad96@gmail.com>
2021-08-31 14:02:53 +0200

    Implement useLocation hook

---
+++ 08-RoutesApp/src/hooks/useLocation.ts
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import Geolocation from '@react-native-community/geolocation';
+import { Location } from '../interfaces/appInterfaces';
+
+export const useLocation = () => {
+  const [hasLocation, setHasLocation] = useState<boolean>(false);
+  const [initialPosition, setInitialPosition] = useState<Location>({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      ({ coords }) => {
+        setInitialPosition({
+          latitude: coords.latitude,
+          longitude: coords.longitude,
+        });
+        setHasLocation(true);
+      },
+      err => console.log('error: ', err),
+      {
+        enableHighAccuracy: true,
+      },
+    );
+  }, []);
+
+  return {
+    hasLocation,
+    initialPosition,
+  };
+};
