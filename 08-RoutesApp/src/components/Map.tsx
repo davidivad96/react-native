034849a3046dd6a0ebec477355eb23e4d5fc2573
davidivad96 <davidivad96@gmail.com>
2021-08-31 14:09:20 +0200

    Use Fab component in Map

--- 08-RoutesApp/src/components/Map.tsx
+++ 08-RoutesApp/src/components/Map.tsx
@@ -4,6 +4,7 @@ import MapView, { Marker as MarkerComponent } from 'react-native-maps';
 import { useLocation } from '../hooks/useLocation';
 import { Marker } from '../interfaces/appInterfaces';
 import { LoadingScreen } from '../pages/LoadingScreen';
+import { Fab } from './Fab';
 
 interface Props {
   markers?: Marker[];
@@ -17,33 +18,45 @@ export const Map = ({ markers = [] }: Props) => {
   }
 
   return (
-    <MapView
-      style={styles.map}
-      showsUserLocation
-      initialRegion={{
-        latitude: initialPosition.latitude,
-        longitude: initialPosition.longitude,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    >
-      {markers.map(marker => (
-        <MarkerComponent
-          key={`${marker.location.latitude},${marker.location.longitude}`}
-          coordinate={{
-            latitude: marker.location.latitude,
-            longitude: marker.location.longitude,
-          }}
-          title={marker.title}
-          description={marker.description}
-        />
-      ))}
-    </MapView>
+    <>
+      <MapView
+        style={styles.map}
+        showsUserLocation
+        initialRegion={{
+          latitude: initialPosition.latitude,
+          longitude: initialPosition.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      >
+        {markers.map(marker => (
+          <MarkerComponent
+            key={`${marker.location.latitude},${marker.location.longitude}`}
+            coordinate={{
+              latitude: marker.location.latitude,
+              longitude: marker.location.longitude,
+            }}
+            title={marker.title}
+            description={marker.description}
+          />
+        ))}
+      </MapView>
+      <Fab
+        iconName="star-outline"
+        onPress={() => {}}
+        style={styles.fabButton}
+      />
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  fabButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
   },
 });
