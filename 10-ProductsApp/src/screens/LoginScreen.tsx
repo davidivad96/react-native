e4788621702beaa342211c2d58cced4ad5f919d3
davidivad96 <davidivad96@gmail.com>
2021-09-01 12:54:08 +0200

    Improve LoginScreen

--- 10-ProductsApp/src/screens/LoginScreen.tsx
+++ 10-ProductsApp/src/screens/LoginScreen.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TextInput } from 'react-native';
+import { Background } from '../components/Background';
+import { WhiteLogo } from '../components/WhiteLogo';
+import { loginTheme } from '../themes/loginTheme';
 
 export const LoginScreen = () => {
   return (
-    <View>
-      <Text>Login screen</Text>
-    </View>
+    <>
+      <Background />
+      <WhiteLogo />
+      <Text style={loginTheme.title}>Login</Text>
+      <Text style={loginTheme.label}>Email:</Text>
+      <TextInput
+        placeholder="Type your email"
+        placeholderTextColor="rgba(255,255,255,0.4)"
+      />
+    </>
   );
 };
