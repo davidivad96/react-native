803ecdb1ed20d8e4d5fbddd6333b384a90903bd7
davidivad96 <davidivad96@gmail.com>
2021-09-01 12:53:53 +0200

    Implement Background component

---
+++ 10-ProductsApp/src/components/Background.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Dimensions, StyleSheet, View } from 'react-native';
+
+const { height, width } = Dimensions.get('window');
+
+export const Background = () => {
+  return <View style={styles.background} />;
+};
+
+const styles = StyleSheet.create({
+  background: {
+    backgroundColor: '#5856D6',
+    position: 'absolute',
+    width: width * 2,
+    height: height * 2,
+    top: -500,
+    transform: [
+      {
+        rotate: '-70deg',
+      },
+    ],
+  },
+});
