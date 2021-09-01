578acebeb722d61de02210dbacdb45b5a712ea49
davidivad96 <davidivad96@gmail.com>
2021-09-01 12:53:47 +0200

    Implement loginTheme

---
+++ 10-ProductsApp/src/themes/loginTheme.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+export const loginTheme = StyleSheet.create({
+  title: {
+    color: '#FFF',
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  label: {
+    marginTop: 25,
+    color: '#FFF',
+    fontWeight: 'bold',
+  },
+});
