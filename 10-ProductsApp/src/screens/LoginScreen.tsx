21f067eea366c061da4f792d896919f5815e73b4
davidivad96 <davidivad96@gmail.com>
2021-09-01 20:13:44 +0200

    Add some text inputs and buttons to LoginScreen

--- 10-ProductsApp/src/screens/LoginScreen.tsx
+++ 10-ProductsApp/src/screens/LoginScreen.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Text, TextInput } from 'react-native';
+import {
+  Platform,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import { Background } from '../components/Background';
 import { WhiteLogo } from '../components/WhiteLogo';
 import { loginTheme } from '../themes/loginTheme';
@@ -8,13 +14,48 @@ export const LoginScreen = () => {
   return (
     <>
       <Background />
-      <WhiteLogo />
-      <Text style={loginTheme.title}>Login</Text>
-      <Text style={loginTheme.label}>Email:</Text>
-      <TextInput
-        placeholder="Type your email"
-        placeholderTextColor="rgba(255,255,255,0.4)"
-      />
+      <View style={loginTheme.formContainer}>
+        <WhiteLogo />
+        <Text style={loginTheme.title}>Login</Text>
+        <Text style={loginTheme.label}>Email:</Text>
+        <TextInput
+          placeholder="Type your email"
+          placeholderTextColor="rgba(255,255,255,0.4)"
+          keyboardType="email-address"
+          underlineColorAndroid="#FFF"
+          style={[
+            loginTheme.inputField,
+            Platform.OS === 'ios' && loginTheme.inputFieldIOS,
+          ]}
+          selectionColor="#FFF"
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Text style={loginTheme.label}>Password:</Text>
+        <TextInput
+          placeholder="******"
+          placeholderTextColor="rgba(255,255,255,0.4)"
+          underlineColorAndroid="#FFF"
+          style={[
+            loginTheme.inputField,
+            Platform.OS === 'ios' && loginTheme.inputFieldIOS,
+          ]}
+          selectionColor="#FFF"
+          autoCapitalize="none"
+          autoCorrect={false}
+          secureTextEntry
+        />
+        <View style={loginTheme.buttonContainer}>
+          <TouchableOpacity activeOpacity={0.4} style={loginTheme.button}>
+            <Text style={loginTheme.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={loginTheme.newUserContainer}>
+          <TouchableOpacity activeOpacity={0.4}>
+            <Text style={loginTheme.buttonText}>New account</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </>
   );
 };
--- 10-ProductsApp/src/themes/loginTheme.ts
+++ 10-ProductsApp/src/themes/loginTheme.ts
@@ -1,6 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 export const loginTheme = StyleSheet.create({
+  formContainer: {
+    flex: 1,
+    paddingHorizontal: 30,
+    justifyContent: 'center',
+    height: 600,
+    paddingBottom: 50,
+  },
   title: {
     color: '#FFF',
     fontSize: 30,
@@ -11,5 +18,33 @@ export const loginTheme = StyleSheet.create({
     marginTop: 25,
     color: '#FFF',
     fontWeight: 'bold',
+  },
+  inputField: {
+    color: '#FFF',
+    fontSize: 20,
+  },
+  inputFieldIOS: {
+    borderBottomColor: '#FFF',
+    borderBottomWidth: 2,
+    paddingBottom: 5,
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  button: {
+    borderWidth: 2,
+    borderColor: '#FFF',
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+  buttonText: {
+    fontSize: 18,
+    color: '#FFF',
+  },
+  newUserContainer: {
+    alignItems: 'flex-end',
+    marginTop: 20,
   },
 });
