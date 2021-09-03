9c8bd4eb62b5a3d1603239aad2978ba6e1f3e853
davidivad96 <davidivad96@gmail.com>
2021-09-03 21:38:09 +0200

    Implement LoadingScreen

---
+++ 10-ProductsApp/src/screens/LoadingScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
+
+export const LoadingScreen = () => (
+  <View style={styles.container}>
+    <ActivityIndicator size={50} color="#000" />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
