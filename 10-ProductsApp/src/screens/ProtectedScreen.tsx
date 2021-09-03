18476f9a33916005ce0d5e49a060229464e8944e
davidivad96 <davidivad96@gmail.com>
2021-09-03 22:13:32 +0200

    Improve ProtectedScreen

--- 10-ProductsApp/src/screens/ProtectedScreen.tsx
+++ 10-ProductsApp/src/screens/ProtectedScreen.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { AuthContext } from '../context/AuthContext';
 
 export const ProtectedScreen = () => {
+  const { user, token, logout } = useContext(AuthContext);
+
   return (
-    <View>
-      <Text>Protected screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Protected screen</Text>
+      <Button title="Logout" color="#5856D6" onPress={logout} />
+      <Text>{JSON.stringify(user, null, 5)}</Text>
+      <Text>{token}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 20,
+  },
+});
