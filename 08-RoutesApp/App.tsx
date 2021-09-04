a853521a9d8c1f0c43e3419a0d31eac603c54f39
davidivad96 <davidivad96@gmail.com>
2021-09-04 13:16:33 +0200

    Create and configure splash screens for Android and iOS

--- 08-RoutesApp/App.tsx
+++ 08-RoutesApp/App.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 import { MainNavigator } from './src/navigation/MainNavigator';
 import { PermissionsProvider } from './src/context/PermissionsContext';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <PermissionsProvider>
       <NavigationContainer>
